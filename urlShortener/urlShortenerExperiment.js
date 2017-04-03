409e436fe5826f9fb8f0fb6f3d1f839d6d6a2243
Divyanshu Singh <dsinecos@gmail.com>
2017-04-03 18:02:35 +0530

    Experimenting to understand SELECT and INSERT commands

--- urlShortener/urlShortenerExperiment.js
+++ urlShortener/urlShortenerExperiment.js
@@ -1,8 +1,29 @@
-// Math.round();
+var pg = require('pg');
 
-var max = 1000000;
-var min = 1;
-var shortenedURLID = Math.round(Math.random()*(max-min) + min);
-var shortenedURL = "bite" + shortenedURLID;
+var conString = "pg://fccbackend:blackwidow@localhost:5432/urlShortener";
+var client = new pg.Client(conString);
 
-console.log(shortenedURL);
+client.connect();
+
+client.query("DROP TABLE IF EXISTS ghanta");
+client.query("CREATE TABLE IF NOT EXISTS ghanta(firstname varchar(64), lastname varchar(64))");
+//client.end();
+//client.connect();
+client.query("INSERT INTO ghanta(firstname, lastname) values($1, $2)", ['Ronald', 'McDonald']);
+client.query("INSERT INTO ghanta(firstname, lastname) values($1, $2)", ['Mayor', 'McCheese']);
+
+var test = "Mayor";
+
+var query = client.query('SELECT * FROM ghanta WHERE firstname = $1', [test]);
+
+query.on("row", function (row, result) {
+    result.addRow(row);
+});
+query.on("end", function (result) {
+    var resultObject = result.rows;
+    console.log(resultObject);
+    console.log(resultObject[0].lastname);
+    client.end();
+});
+
+console.log("Hoye");
